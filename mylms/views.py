01e786b43ed16e91da72232bde04d965a79388ea
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-03-13 20:25:04 +0300

    changed structure of the build, added dist folder

--- mylms/views.py
+++ mylms/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html')  # Render a template named 'home.html'
 
 
 # Create your views here.
