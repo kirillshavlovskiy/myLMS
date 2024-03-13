4d79dff59fb5f5665cce27ab52f1cd12f925b50f
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-03-13 21:47:22 +0300

    changed structure of the build, added dist folder

--- mylms/settings.py
+++ mylms/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mylms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- mylms/urls.py
+++ mylms/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+
 from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
-    path('', home_view, name='home'),
+    path('brainpower', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
